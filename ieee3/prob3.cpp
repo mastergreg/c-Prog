6dd590ad1ade6696e916be918c591942f345798e
Greg Liras <gregliras@gmail.com>
2011-03-28 17:04:30 +0300

    changed the code for problem3

--- ieee3/prob3.cpp
+++ ieee3/prob3.cpp
@@ -12,17 +12,15 @@ int find_pal(unsigned int lim,string mystr);
 
 int main()
 {
-  unsigned int limit=6795;
+  unsigned int limit=7074;
   int i,maxi;
   string line,testline;
   ifstream myfile("endo.dna");
   getline (myfile,line);
   maxi=line.size()/768;
   for(i=0;i<768;i++)
-
   {
     testline=line.substr(i*maxi,maxi);
-    cout << testline.size() << endl;
     while (testline.size()>limit)
     {
       limit = max((int)limit,find_pal(limit,testline)); 
@@ -31,18 +29,18 @@ int main()
 
     cout << "i= "<<i<<endl;
   }
-  for(i=0;i<768;i++)
-
-  {
-    testline=line.substr(i*maxi+maxi/2,maxi);
-    while (testline.size()>limit)
-    {
-      limit = max((int)limit,find_pal(limit,testline)); 
-      testline.erase(testline.begin());
-    }
-
-    cout << "i= "<<i<<endl;
-  }
+  //  for(i=0;i<768;i++)
+  //
+  //  {
+  //    testline=line.substr(i*maxi+maxi/2,maxi);
+  //    while (testline.size()>limit)
+  //    {
+  //      limit = max((int)limit,find_pal(limit,testline)); 
+  //      testline.erase(testline.begin());
+  //    }
+  //
+  //    cout << "i= "<<i<<endl;
+  //  }
   cout << "the limit is " << limit << endl;
   //  testline=line.substr(0,line.size()/8);
   //  limit=locate_pals(testline);
@@ -52,17 +50,20 @@ int main()
 int find_pal(unsigned int lim,string mystr)
 {
   unsigned int a=mystr.size();
+  ofstream outfile("thestring");
   while (a>=lim)
   {
     if (ispalindrome(mystr.c_str()))
     {
+      cout << mystr.c_str() << endl;
+      outfile << mystr.c_str() << endl << endl;
       return a;
     }
     else
     {
       mystr.erase(mystr.end()-1);
+      a=mystr.size();
     }
-    a=mystr.size();
   }
   return 0;
 }
