4c44ace1d864cf3b03bc255a538448e83783fe38
Greg Liras <gregliras@gmail.com>
2011-04-09 13:58:44 +0300

    maximum recursion level

--- ieee2.1/prob2.1.cpp
+++ ieee2.1/prob2.1.cpp
@@ -1,89 +1,46 @@
-#include "palindrome.h"
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
 #include <string>
-#include <algorithm>
-#include <vector>
+#include <string.h>
+
 using namespace std;
-string make_teststring(string line,string astr);
-vector< vector<int> > c(101, vector<int>(101,0));
-
-int LCS(string X,string Y)
-{
-  if (Y.length() > X.length())
-    swap(X,Y);
-  int m = X.length(),n=Y.length();
-  vector< vector<int> > c(2, vector<int>(n+1,0));
-  int i,j;
-  for (i=1;i<=m;i++)
-  {
-    for (j=1;j<=n;j++)
-    {
-      if (X[i-1]==Y[j-1])
-      {
-        c[1][j]=c[0][j-1]+1;
-        break;
-      }
-      else
-      {
-        c[1][j]=max(c[1][j-1],c[0][j]);
-        break;
-      }
-    }
-    cout << "hi"<<endl;
-//    for (j=1;j<=n;j++)
-//      c[0][j]=c[1][j];
-  }
-  return (c[1][n]);
-}
-int maxIndex(int a[], int size);
-
-int find_range(string fline,char c);
+unsigned int lcs_len(char *,char *);
+unsigned int recursion_level=0;
+char *xinit,*ystop;
 int main()
 {
-  size_t maxi;
-  string anotherstring="",line,fileline;
+  unsigned int maxi;
+  string line;
   ifstream myfile("softlab.txt");
-  getline (myfile,fileline);
-  maxi=fileline.length();
-  line=fileline;
-  reverse(line.begin(),line.end());
-
-
-  cout <<  LCS(line,fileline) << endl; 
-
-
+  getline (myfile,line);
+  maxi=line.size();
+  char *xinit=new (nothrow) char[maxi];
+  strncpy(xinit,line.c_str(),maxi);
+  ystop=xinit+(maxi)*sizeof(char);
+  cout << lcs_len(ystop,xinit) <<endl;
+  return 0;
 }
 
-int maxIndex(int a[], int size)
+
+unsigned int lcs_len(char *xi,char *yi)
 {
-  int maxIndex = 0;
-  for (int i=1; i<size; i++) 
+  unsigned int counter = 0,a,b;
+  cout << ++recursion_level << endl;
+  while (xi>xinit && yi<ystop)
   {
-    if (a[i] > a[maxIndex]) 
+    if (*xi==*yi)
     {
-      maxIndex = i;
+      counter++;
+      xi--;
+      yi++;
+    }
+    else
+    {
+      a=lcs_len(--xi,yi);
+      b=lcs_len(xi,--yi);
+      counter += (b<a)?a:b;
     }
   }
-  return maxIndex;
+  return counter;
 }
-
-int find_range(string fline,char c)
-{
-  return fline.rfind(c)-fline.find(c);
-}
-
-string make_teststring(string line,string astr)
-{
-  int i;
-  string newstr="";
-  for (i=0;i<52;i++)
-  {
-    if ((int)line.find(astr[i])!=-1) newstr+=astr[i];
-  }
-  cout << newstr << endl;
-  return newstr;
-}
-
-
