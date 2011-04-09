0dcd7466c10f7f8030120498512a5d263ff4e063
Greg Liras <gregliras@gmail.com>
2011-04-09 21:53:17 +0300

    move to desktop

--- ieee2.1/prob2.1.cpp
+++ ieee2.1/prob2.1.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 unsigned int lcs_len(char *,char *);
+unsigned int lcs_len_slow(char *chi,char *chj,unsigned int max);
 unsigned int recursion_level=0;
 char *xinit,*ystop;
 int main()
@@ -18,18 +19,45 @@ int main()
   char *xinit=new (nothrow) char[maxi];
   strncpy(xinit,line.c_str(),maxi);
   ystop=xinit+(maxi)*sizeof(char);
-  cout << lcs_len(ystop,xinit) <<endl;
+  cout << lcs_len_slow(xinit,ystop,maxi) <<endl;
   return 0;
+}
+
+
+
+unsigned int lcs_len_slow(char *xi,char *xj,unsigned int max)
+{
+  unsigned int i,j;
+  unsigned int row[2][max+1];
+  char *chi=xi,*chj=xj;
+  for (i=1;i<max;i++)
+  {
+    for (j=1;j<max;j++)
+    {
+      if (chi++==chj)
+      {
+        row[(i+1)%2][j]=row[i%2][j]++;
+      }
+      else
+      {
+        row[(i+1)%2][j]=( row[(i+1)%2][j-1]<row[i%2][j] ) ? row[i%2][j]:row[(i+1)%2][j-1];
+      }
+    }
+    chi=xi;
+    chj--;
+  }
+  return (row[0][max]<row[1][max] ) ? row[1][max]:row[0][max];
 }
 
 
 unsigned int lcs_len(char *xi,char *yi)
 {
   unsigned int counter = 0,a,b;
-  while (xi>=xinit && yi<=ystop)
+  while (xi>xinit && yi<ystop)
   {
     if (*xi==*yi)
     {
+      counter++;
       xi--;
       yi++;
     }
